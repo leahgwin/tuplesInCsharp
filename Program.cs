390317e742578f2362d7f1ca032b5688505799c1
leahgwin <leahgwin@gmail.com>
2018-08-22 14:52:49 -0500

    finished exercise

--- Program.cs
+++ Program.cs
@@ -14,13 +14,25 @@ namespace tuples
             (string product, double amount, int quantity) nailGun = ("Nail Gun", 189.99, 1);
             (string product, double amount, int quantity) bandaids = ("Band Aids", 5.99, 50);
             (string product, double amount, int quantity) staples = ("Staples", 3.99, 125);
+            //add to my list
+            transactions.Add(hammer);
+            transactions.Add(nailGun);
+            transactions.Add(nails);
+            transactions.Add(bandaids);
+            transactions.Add(staples);
 
+            //making these variables to call later
             int TotalProductsSold = 0;
+            //has to be a double since there's decimals involved.
+            double TotalRevenue = 0;
 
             foreach ((string product, double amount, int quantity) t in transactions)
             {
                 //logic goes here to look up quantity and amount in each transaction
+                TotalProductsSold += t.quantity;
+                TotalRevenue += t.quantity * t.amount;
             }
+            Console.WriteLine($"We sold {TotalProductsSold} products for a total of ${TotalRevenue}.");
         }
     }
 }
